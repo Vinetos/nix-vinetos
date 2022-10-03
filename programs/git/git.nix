09dcba0ce83271be773e5aca0aa72fb26cccf2da
Valentin Chassignol <Vinetosdev@gmail.com>
2022-10-03 14:30:36 +0200

    feat: Sign commit with pgp key

--- programs/git/git.nix
+++ programs/git/git.nix
@@ -1,7 +1,17 @@
 {
   enable = true;
+
+  # User config
   userName = "Valentin Chassignol";
   userEmail = "Vinetosdev"+"@"+"gmail"+"."+"com";
+
+  # Signing Module
+  signing = {
+    key = "D40082CCAF4E61A4";
+    signByDefault = true;
+  };
+
+  # Extra Config
   extraConfig = {
     core = {
       editor = "vim";
@@ -10,4 +20,5 @@
       defaultBranch = "main";
     };
   };
+  
 }
