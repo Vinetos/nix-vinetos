7048400f6dce5f5409f004f67ddc02b24bee9db6
Valentin <Vinetos@users.noreply.github.com>
2022-10-25 19:49:20 +0200

    feat(fish): Add Postgres Create Database alias (#3)

--- programs/fish/fish.nix
+++ programs/fish/fish.nix
@@ -5,7 +5,18 @@
   interactiveShellInit = ''
     ${pkgs.any-nix-shell}/bin/any-nix-shell fish --info-right | source
   '';
+  
+  shellInit = ''
+    # Postgres Create Database
+    pcb() {
+        psql "postgresql://postgres:postgres@localhost:5432/postgres" -c 'CREATE DATABASE "$1"'
+    }
+  '';
+  
   shellAliases = {
+      # Nix
+      ns = "nix-shell -p "
+      # Java
       sjh = "set JAVA_HOME ~/.nix-profile/lib/openjdk/";
       gsi = "git submodule update --init --recursive";
   };
