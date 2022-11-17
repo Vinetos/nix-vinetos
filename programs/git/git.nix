eb4c70fe3d02b4e9a258020c4952051efe62c561
Valentin Chassignol <Vinetosdev@gmail.com>
2022-11-17 18:28:33 +0100

    feat: git: Add alias

--- programs/git/git.nix
+++ programs/git/git.nix
@@ -1,3 +1,5 @@
+{ pkgs, ...}:
+
 {
   enable = true;
 
@@ -15,9 +17,19 @@
   extraConfig = {
     core = {
       editor = "vim";
+      core.pager = "${pkgs.delta}/bin/delta --dark";
     };
     init.defaultBranch = "main";
     pull.rebase = true;
+  };
+
+  # Alias
+  aliases = {
+      # Better log
+      l =  "log --graph --pretty='%Cred%h%Creset - %C(bold blue)<%an>%Creset %s%C(yellow)%d%Creset %Cgreen(%cr)' --abbrev-commit --date=relative";
+      # Branch create from $2 and create tracked branch
+      bc = "!sh -c 'git switch -c \"$1\" \"$2\" && git push --set-upstream origin \"$1\"' -";
+      bd = "!sh -c 'git switch main && git push -d origin \"$1\" && git branch -d \"$1\"' -";
   };
   
 }
